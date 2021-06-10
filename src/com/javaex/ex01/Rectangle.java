304290b55592a9c5615a640c5af170e2fc946fbd
olseason <sjlee2177@naver.com>
2021-06-11 01:40:43 +0900

    06/10_practice07 과제

---
+++ src/com/javaex/ex01/Rectangle.java
@@ -0,0 +1,41 @@
+package com.javaex.ex01;
+
+public class Rectangle extends Shape {
+	
+	private int width;
+	private int height;
+	
+	
+	public Rectangle(String fillColor) {
+		super.fillColor = fillColor;
+	}
+	
+	
+	public Rectangle(String fillColor, int width, int height){
+		super(fillColor);
+		this.width=width;
+		this.height=height;
+	}
+	
+	public int GetWidth() {
+		return this.width;
+	}
+	
+	
+	public void showInfo() {
+		System.out.println();
+	}
+	
+	public void draw(){
+		System.out.println("[사각형]#면색:" +super.fillColor+ " "
+				+ " #가로:" + width 
+				+" #세로:" + height + "그렸습니다.");                                                                        
+	}
+	
+	
+	
+		
+
+}
+
+
